054782fb904e390f4c72a0b7df47eeda49182bcc
ntnguyen041 <nt.nguyen041@gmail.com>
2026-06-09 20:33:55 +0700

    add code

---
+++ PhanSo.cs
@@ -0,0 +1,101 @@
+﻿namespace PhanSoApp
+{
+    public class PhanSo
+    {
+        public int TuSo { get; private set; }
+        public int MauSo { get; private set; }
+
+        public PhanSo(int tuSo, int mauSo)
+        {
+            if (mauSo == 0)
+            {
+                throw new ArgumentException("Mau so khong duoc bang 0!");
+            }
+            if (mauSo < 0) { tuSo = -tuSo; mauSo = -mauSo; }
+            int ucln = UCLN(Math.Abs(tuSo), mauSo);
+            TuSo = tuSo / ucln;
+            MauSo = mauSo / ucln;
+        }
+        private static int UCLN(int a, int b)
+        {
+            while (b != 0) { int t = b; b = a % b; a = t; }
+            return a;
+        }
+        public PhanSo RutGon()
+        {
+            return new PhanSo(TuSo, MauSo); 
+        }
+
+        public override string ToString()
+        {
+            if (MauSo == 1) return TuSo.ToString();
+            return $"{TuSo}/{MauSo}";
+        }
+        public static PhanSo operator +(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(
+                a.TuSo * b.MauSo + b.TuSo * a.MauSo,
+                a.MauSo * b.MauSo
+            );
+        }
+        public static PhanSo operator -(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(
+                a.TuSo * b.MauSo - b.TuSo * a.MauSo,
+                a.MauSo * b.MauSo
+            );
+        }
+        public static PhanSo operator *(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(
+                a.TuSo *b.TuSo,
+                a.MauSo * b.MauSo
+            );
+        }
+        public static PhanSo operator /(PhanSo a, PhanSo b)
+        {
+            return new PhanSo(
+                a.TuSo * b.MauSo,
+                a.MauSo * b.TuSo
+            );
+        }
+        public static PhanSo operator +(int n, PhanSo ps)
+        {
+            return new PhanSo(n, 1) + ps;
+        }
+        public static PhanSo operator +(PhanSo ps, int n)
+        {
+            return new PhanSo(n, 1) + ps;
+        }
+
+        public static bool operator ==(PhanSo a, PhanSo b)
+        {
+            return a.TuSo == b.TuSo &&
+                   a.MauSo == b.MauSo;
+        }
+        public static bool operator !=(PhanSo a, PhanSo b)
+        {
+            return !(a == b);
+        }
+        public static bool operator >(PhanSo a, PhanSo b)
+        {
+            return (long)a.TuSo*b.MauSo >(long) b.TuSo * a.MauSo;
+        }
+        public static bool operator <(PhanSo a, PhanSo b)
+        {
+            return (long)a.TuSo * b.MauSo < (long)b.TuSo * a.MauSo;
+        }
+        public static bool operator >=(PhanSo a, PhanSo b)
+        {
+            return a<b||a==b;
+        }
+        public static bool operator <=(PhanSo a, PhanSo b)
+        {
+            return a>b||a==b;
+        }
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using PhanSoApp;
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<PhanSo> pS = new List<PhanSo>
+{
+    new PhanSo(1, 2),
+    new PhanSo(1, -3),
+   // new PhanSo(1, 0)
+};
+foreach (PhanSo p in pS)
+{
+    try {  
+        Console.WriteLine(p.ToString());
+    } catch(ArgumentException ex) { 
+        Console.WriteLine(ex.Message);
+    }
+
+}
+
+//// bai 2
+PhanSo ps1 = new PhanSo(1, 2);
+PhanSo ps2 = new PhanSo(1, 3);
+Console.WriteLine($"ps1 = {ps1}");
+Console.WriteLine($"ps2 = {ps2}");
+Console.WriteLine($"ps1 + ps2 = {ps1 + ps2}");
+Console.WriteLine($"ps1 - ps2 = {ps1 - ps2}");
+Console.WriteLine($"ps1 * ps2 = {ps1 * ps2}");
+Console.WriteLine($"ps1 / ps2 = {ps1 / ps2}");
+//Console.WriteLine($"ps1 + 2 = {ps1 + 2}");
+/// bai 3 Thêm vào lớp PhanSo các toán tử so sánh. Lưu ý quy tắc “đi theo cặp” bắt buộc của C#.
+
+PhanSo ps_1 = new PhanSo(1, 2);   
+PhanSo ps_2 = new PhanSo(2, 4);  
+PhanSo ps_3 = new PhanSo(1, 3);  
+
+
+Console.WriteLine(ps_1 == ps_2);  
+Console.WriteLine(ps_1 != ps_3); 
+Console.WriteLine(ps_3 < ps_1);  
+Console.WriteLine(ps_1 > ps_3);   
+
+
+
+
+app.MapGet("/", () => "PhanSoApp !");
+app.Run();
